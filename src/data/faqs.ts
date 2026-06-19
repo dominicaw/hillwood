07a3e481d34575b644404e8042fec6d38dccaefa
Dominica Wannenburg <dominica.wannenburg@tillo.io>
2026-06-19 16:41:02 +0200

    FAQs

--- src/data/faqs.ts
+++ src/data/faqs.ts
@@ -6,28 +6,27 @@ export interface FAQ {
 
 export const faqs: FAQ[] = [
   {
-    id: 'contact',
-    question: 'How do I contact the managing agent?',
-    answer: 'Add your answer here.',
-  },
-  {
     id: 'internet',
     question: 'Who is the internet service provider?',
-    answer: 'Add your answer here.',
+    answer:
+      'Frogfoot is the internet network operator for Hillwood. You can choose any internet service provider you like (Mweb, Afrihost, etc).',
   },
   {
     id: 'refuse',
     question: 'When is refuse collected?',
-    answer: 'Add your answer here.',
+    answer:
+      'Thursday mornings. Our groundsman will put the bins out for collection.',
   },
   {
-    id: 'levies',
-    question: 'How and when are levies paid?',
-    answer: 'Add your answer here.',
+    id: 'gardens',
+    question: 'Who maintains the gardens and common areas?',
+    answer:
+      'We have a groundsman who attends to the property three times a week.',
   },
   {
     id: 'visitors',
     question: 'Where can visitors park?',
-    answer: 'Add your answer here.',
+    answer:
+      'There is unfortunately no parking available for visitors. Please arrange for your visitors to park in the surrounding streets.',
   },
 ]
